22a454207fb2ca10798981e2eec03a2b06d95da8
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-06-06 15:06:03 -0400

    Homemade virtualenvwrapper for pyvenv-3.4

---
+++ bin/python_funcs.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+VENV_DIR="$HOME/.venvs"
+# Activate venv and re-source PS1 from bash_prompt to get colored venv name.
+function work() {
+    source "$VENV_DIR/$1/bin/activate"
+    source "$HOME/.bash_prompt"
+}
+
+# Pyvenv-3.4 is currently broken in Ubuntu 14.04. This script needs to be run to create new
+# environments with pyvenv
+function pyv() {
+    if [ -d $VENV_DIR ]; then
+        echo "Making directory for environments in $VENV_DIR"
+        mkdir $VENV_DIR
+    fi
+    venv="$VENV_DIR/$1"
+    echo "Creating virtual environment in $venv"
+    if ! (pyvenv-3.4 --without-pip $venv); then
+        echo "Something wrong happened"
+        return
+    fi
+    source "$venv/bin/activate"
+    curl https://bootstrap.pypa.io/get-pip.py | python
+    deactivate
+    work $1
+}
+
+function delete_venv() {
+    rm -rf "$VENV_DIR/$1"
+}
