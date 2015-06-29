2c06b52ea9b392692b6ca3fc6ca9d581404ba3a0
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-06-29 09:01:26 -0400

    Add less/grep inline aliases
    Reduces "ls | grep foo | less" to "ls G foo L"

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,6 @@
 alias reload!='. ~/.zshrc'
+
+# Inline aliases, zsh -g aliases can be anywhere in command line
+alias -g G='| grep -'
+alias -g L='| less'
+# Reduces "ls | grep foo | less" to "ls G foo L"
