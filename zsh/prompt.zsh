950e27b8098c1483c89a5038e3cb95a75f305ccd
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-07-03 18:22:31 -0400

    Prompt update

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -183,8 +183,8 @@ function virtualenv_info ()
   [ $VIRTUAL_ENV ] && echo '['`basename $VIRTUAL_ENV`'] '
 }
 export VIRTUAL_ENV_DISABLE_PROMPT=1
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[blue]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%}"
+ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg[blue]%}("
+ZSH_THEME_GIT_PROMPT_SUFFIX="%{$fg[blue]%})%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[red]%}✗"
 # ZSH_THEME_GIT_PROMPT_UNTRACKED="%{$fg[yellow]%} [!]"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[green]%}✓"
