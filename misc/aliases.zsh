b445e69290f52cd27f484314cb02ed9c580eba26
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-08-23 21:08:50 -0400

    Scripts and aliases for chrome search and browse

--- misc/aliases.zsh
+++ misc/aliases.zsh
@@ -1,2 +1,6 @@
 # Depends on bin/activate-chrome
 alias c="activate-chrome > /dev/null 2>&1"
+# Depends on bin/activate-chrome and bin/google
+alias g="google > /dev/null 2>&1"
+# Depends on bin/activate-chrome
+alias 5000="c http://localhost:5000"
