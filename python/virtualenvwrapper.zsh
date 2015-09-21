1e6e32698b3c174199e25c2c5fb7d88994f536a2
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-09-21 16:37:11 -0400

    detect virtualenvwrapper

--- python/virtualenvwrapper.zsh
+++ python/virtualenvwrapper.zsh
@@ -1,3 +1,6 @@
-export WORKON_HOME=~/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2
-source /usr/local/bin/virtualenvwrapper.sh
+venvwrapper="/usr/local/bin/virtualenvwrapper.sh"
+if [ -e $venvwrapper ]; then
+    export WORKON_HOME=~/.virtualenvs
+    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
