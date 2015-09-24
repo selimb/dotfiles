8df73de9495a43fc0953476edd14c2a0f19f6046
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-09-23 23:19:27 -0400

    Block comments across the whole line

--- sublime/User/BlockComments.py
+++ sublime/User/BlockComments.py
@@ -8,32 +8,42 @@ class PromptBlockCommentCommand(sublime_plugin.WindowCommand):
                                                  on_done=self.on_done,
                                                  on_change=None,
                                                  on_cancel=None)
-    def on_done(self, title):
-        try:
-            title = str(title)
-            if self.window.active_view():
-                self.window.active_view().run_command("block_comment",
-                                                      {"title": title})
-        except ValueError:
-            pass
+    def on_done(self, input):
+        parts = input.split(';')
+        if len(parts) == 1:
+            length = 0
+        else:
+            length = int(parts[1])
+        title = parts[0].strip()
+        view = self.window.active_view()
+        if view:
+            view.run_command(
+                "block_comment",
+                {"title": title, "length": length}
+            )
 
 
 class BlockCommentCommand(sublime_plugin.TextCommand):
 
-    def run(self, edit, title):
-        print("Hi")
+    def run(self, edit, title, length):
         print(self.view.get_regions("comment"))
-        block = self.format_block(title)
+        block = self.format_block(title, length)
         self.view.insert(edit, self.view.sel()[0].begin(),
                          block)
 
-    def format_block(self, title):
+    def format_block(self, title, length):
         comment_char = get_comment_character(self.view)
         indent = get_indent(self.view)
         title_length = len(title)
-        tot_length = title_length + 6
+        if length == 0:
+            tot_length = 80
+        else:
+            tot_length = title_length + length
         border = comment_char * tot_length
-        mid = "{0}{0} {1} {0}{0}".format(comment_char, title)
+        mid = "{0}{0}{1}{0}{0}".format(
+            comment_char,
+            title.center(tot_length - 4)
+        )
         return '\n'.join((border, indent + mid, indent + border, indent))
 
 
