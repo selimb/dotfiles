75b582fea055e47cfc20f5ceb00aeb75ef47f0e8
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-10-06 10:06:45 -0400

    dont need this

--- python/virtualenvwrapper.zsh
+++
@@ -1,6 +0,0 @@
-venvwrapper="/usr/local/bin/virtualenvwrapper.sh"
-if [ -e $venvwrapper ]; then
-    export WORKON_HOME=~/.virtualenvs
-    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2
-    source /usr/local/bin/virtualenvwrapper.sh
-fi
