118bbe9deb26a09d93177c99913c28c8a93e3187
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-11-04 12:40:26 -0500

    Add function to find variable declaration in syn3d

--- guillimin/functions.zsh
+++ guillimin/functions.zsh
@@ -3,3 +3,21 @@ grepf () {
     echo "$1"
     grep -nHIi "$1" *.f
 }
+
+# Grep fortran files for a variable assignment
+# Grep options: -n Show line number
+#               -H With filename
+#               -I Don't process binary files
+#               -i Ignore case
+#               -P Perl RegEx
+findvar () {
+    grepopts="-nHIiP"
+    pattern="\s+$1\s*(?:\(.*=|=)"
+    shift;
+    if [ $1 ]; then
+        results=$(grep "$grepopts" "$pattern" $*)
+    else
+        results=$(grep "$grepopts" "$pattern" *.f)
+    fi
+    echo "$results"
+}
