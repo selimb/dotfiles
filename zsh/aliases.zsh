c7bf4f7d29fdc62cf102ac39b82ee1fa02948320
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-11-04 12:40:40 -0500

    Fix G global alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,7 +3,7 @@ alias reload!='. ~/.zshrc'
 # Inline aliases, zsh -g aliases can be anywhere in command line
 
 # Reduces "ls | grep foo | less" to "ls G foo L"
-alias -g G='| grep -'
+alias -g G='| grep'
 alias -g L='| less'
 
 # Copy to clipboard
