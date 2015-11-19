0d6cf10a38eaf0e37434662b40f0d0289c382c94
Selim Belhaouane <selim.belhaouane@gmail.com>
2015-11-19 13:57:41 -0500

    change home alias to go into source code directly

--- guillimin/aliases.zsh
+++ guillimin/aliases.zsh
@@ -2,4 +2,4 @@
 alias guigui='ssh selimb@guillimin.clumeq.ca'
 
 # cd to CFDLab's project directory
-alias home='cd /sb/project/rck-371-aa/selimb'
+alias home='cd /sb/project/rck-371-aa/selimb/syn3d/src'
