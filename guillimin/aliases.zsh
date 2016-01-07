601f242de1ee6e8e05fd716f13e2a08f5b6afa16
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-01-07 12:42:40 -0500

    guigui ftp alias

--- guillimin/aliases.zsh
+++ guillimin/aliases.zsh
@@ -1,5 +1,8 @@
 # Connect to Guillimin
 alias guigui='ssh selimb@guillimin.clumeq.ca'
 
+# SFTP to Guillimin
+alias guiguiftp='sftp selimb@guillimin.clumeq.ca'
+
 # cd to CFDLab's project directory
 alias home='cd /sb/project/rck-371-aa/selimb/syn3d/src'
