2b97780ecabb8585c73857cc4d4c6b92de041b84
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-01-30 16:15:36 -0500

    Fix guillimin detection bug

--- guillimin/env.zsh
+++ guillimin/env.zsh
@@ -4,10 +4,10 @@ local hostname=$(hostname)
 local pattern="[a-z]{2}-[0-9][a-z][0-9]{2}-[a-z][0-9]{2}"
 if $(echo $hostname | grep -E $pattern &> /dev/null); then
     export GUILLIMIN=1
-fi
-# Not on login node
-if [ $hostname[0,2] != "lg" ]; then
-    export GUILLIMIN_JOB_NODE=1
+    # Not on login node
+    if [ $hostname[0,2] != "lg" ]; then
+        export GUILLIMIN_JOB_NODE=1
+    fi
 fi
 
 # PYTHONPATH
