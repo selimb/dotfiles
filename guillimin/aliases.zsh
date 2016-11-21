85c47a48503ab8c66a0e1c7f70903033207da022
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-11-21 12:43:36 -0500

    sftp alias automatically cd into syn3d dir

--- guillimin/aliases.zsh
+++ guillimin/aliases.zsh
@@ -2,5 +2,5 @@
 alias guigui='ssh selimb@guillimin.clumeq.ca'
 
 # SFTP to Guillimin
-alias guiguiftp='sftp selimb@guillimin.clumeq.ca'
+alias guiguiftp='sftp selimb@guillimin.clumeq.ca:/sb/project/rck-371-aa/selimb/syn3d'
 
