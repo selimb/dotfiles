de89dba6f47edaaa98be0af3ccd4db961ef56d88
Selim Belhaouane <selim.belhaouane@gmail.com>
2016-11-21 12:40:55 -0500

    Better python install scripts

--- python/install.sh
+++ python/install.sh
@@ -1,10 +1,21 @@
+py2=python2
+if ! command -v $py2 >/dev/null; then
+    py2=python
+fi
+
+if ! command -v pip >/dev/null; then
+    echo "Downloading pip"
+    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+    sudo $py2 get-pip.py
+    rm get-pip.py
+fi
+if ! command -v virtualenv >/dev/null; then
+    echo "Installing virtualenv"
+    sudo pip install virtualenv
+fi
 if [ "$PIPSI_HOME" -a "$PIPSI_BIN_DIR" ]; then
-    echo "Downloading pipsi"
-    wget https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py > /dev/null
-    echo "Installing pipsi."
-    python2 get-pipsi.py > /dev/null
-    echo "Removing installation file."
-    rm get-pipsi.py > /dev/null
+    echo "Installing pipsi"
+    curl https://raw.githubusercontent.com/mitsuhiko/pipsi/master/get-pipsi.py | $py2
 else
     echo "Set $PIPSI_HOME and $PIPSI_BIN_DIR before running this."
 fi
