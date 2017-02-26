cf2fe2f0bf9a06ad1f8655d96b562e6de370e279
Selim Belhaouane <selim.belhaouane@gmail.com>
2017-02-26 14:22:24 -0500

    os x stuff

--- guillimin/aliases.zsh
+++ guillimin/aliases.zsh
@@ -4,3 +4,5 @@ alias guigui='ssh selimb@guillimin.clumeq.ca'
 # SFTP to Guillimin
 alias guiguiftp='sftp selimb@guillimin.clumeq.ca:/sb/project/rck-371-aa/selimb/syn3d'
 
+# Connect to my computer at school
+alias school='ssh selimb@wheel.mecheng.mcgill.ca'
