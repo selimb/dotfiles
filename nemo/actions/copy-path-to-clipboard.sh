d114b6f64771697f61bc374a172fa429131d1d87
Selim Belhaouane <selim.belhaouane@gmail.com>
2023-09-06 09:55:28 -0400

    nemo again

--- nemo/actions/copy-path-to-clipboard.sh
+++ nemo/actions/copy-path-to-clipboard.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+echo -n "$*" >>/home/selimb/nemo.log
 
 IFS=$'\n'
 echo -n "$*" | xclip -sel clipboard
 
-(( $# > 1 )) && s=s || s=
+(($# > 1)) && s=s || s=
 notify-send "Nemo" "$# path$s copied to the system clipboard"
